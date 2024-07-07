3b7dbe17d936329ebd23241d0c6740665a61ff32
Pinku <pinkuparmer96@gmail.com>
2024-07-07 19:31:41 +0530

    first commit

---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import './nav.css'
+function Nav() {
+  return (
+    <nav className='nav'>
+        <h1 className='heading'>Password Generator</h1>
+      </nav>
+  )
+}
+
+export default Nav
